d8183e0da9ac9766d48639d792241fc420131ed6
Manop Kongoon <kongoon@hotmail.com>
2015-08-28 10:28:17 +0700

    add role

--- console/controllers/RbacController.php
+++ console/controllers/RbacController.php
@@ -82,4 +82,60 @@ class RbacController extends Controller
         
         echo 'Create Permission success!';
     }   
+    public function actionCreateRole()
+    {
+        //yii rbac/create-role
+        $auth = Yii::$app->authManager;
+        
+        // mission
+        $mission_index = $auth->createPermission('mission/mission/index');
+        $mission_create = $auth->createPermission('mission/mission/create');
+        $mission_update = $auth->createPermission('mission/mission/update');
+        $mission_delete = $auth->createPermission('mission/mission/delete');
+        $mission_view = $auth->createPermission('mission/mission/view');
+
+        // personal
+        $personal_index = $auth->createPermission('personal/personal/index');
+        $personal_create = $auth->createPermission('personal/personal/create');
+        $personal_update = $auth->createPermission('personal/personal/update');
+        $personal_delete = $auth->createPermission('personal/personal/delete');
+        $personal_view = $auth->createPermission('personal/personal/view');
+  
+        // department
+        $department_index = $auth->createPermission('setting/department/index');
+        $department_create = $auth->createPermission('setting/department/create');
+        $department_update = $auth->createPermission('setting/department/update');
+        $department_delete = $auth->createPermission('setting/department/delete');
+        $department_view = $auth->createPermission('setting/department/view');
+
+        
+        $user = $auth->createRole('user');
+        $auth->add($user);
+        $auth->addChild($user, $mission_index);
+        $auth->addChild($user, $mission_view);
+        $auth->addChild($user, $personal_index);
+        $auth->addChild($user, $personal_view);
+        $auth->addChild($user, $department_index);
+        $auth->addChild($user, $department_view);
+        
+        $staff = $auth->createRole('staff');
+        $auth->add($staff);
+        $auth->addChild($staff, $mission_create);
+        $auth->addChild($staff, $mission_update);
+        $auth->addChild($staff, $personal_create);
+        $auth->addChild($staff, $personal_update);
+        $auth->addChild($staff, $department_create);
+        $auth->addChild($staff, $department_update);
+        $auth->addChild($staff, $user);
+        
+        
+        
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $mission_delete);
+        $auth->addChild($admin, $personal_delete);
+        $auth->addChild($admin, $department_delete);
+        $auth->addChild($admin, $staff);
+        echo 'Create Role success!';
+    }
 }
